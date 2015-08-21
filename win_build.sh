256fbc2012d5b20630dfcec553b6b050534e405f
Damian Bogel <kele@google.com>
2015-08-21 13:06:53 +0000

    Build gnulib only when it doesn't exist.

--- win_build.sh
+++ win_build.sh
@@ -1,8 +1,12 @@
 set -e
 
-./gnulib_build.sh
+if [ -d "__build_gnulib" ]; then
+    echo "Assuming that gnulib is built already, because __build_gnulib exists."
+else
+    ./gnulib_build.sh
+fi
 
-export GNULIB_DIR=`pwd`/__build_gnulib/__build
+export GNULIB_DIR=`pwd`/__build_gnulib/__build/gllib
 
 ./build.sh
 ./configure --disable-all-plugins --enable-logfile CFLAGS="-I$GNULIB_DIR" LDFLAGS="-L$GNULIB_DIR" LIBS="-lgnu" --host="mingw32"
