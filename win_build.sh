53903807c88b109226ff4e7a59bc9fe1d5c4f347
Damian Bogel <kele@google.com>
2015-08-21 18:04:09 +0000

    Changed the collectd prefix for ./configure.

--- win_build.sh
+++ win_build.sh
@@ -9,6 +9,6 @@ fi
 export GNULIB_DIR=`pwd`/__build_gnulib/_build/gllib
 
 ./build.sh
-./configure --disable-all-plugins --enable-logfile CFLAGS="-I$GNULIB_DIR" LDFLAGS="-L$GNULIB_DIR" LIBS="-lgnu" --host="mingw32"
+./configure --prefix="C:/opt" --disable-all-plugins --enable-logfile CFLAGS="-I$GNULIB_DIR" LDFLAGS="-L$GNULIB_DIR" LIBS="-lgnu" --host="mingw32"
 cp my_libtool libtool
 make
