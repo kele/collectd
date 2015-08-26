b8e3bfde9a69a598920008970e3b1d198af8d731
Damian Bogel <kele@google.com>
2015-08-26 18:25:53 +0000

    Reformatting win_build.sh. No relevant changes.

--- win_build.sh
+++ win_build.sh
@@ -10,7 +10,11 @@ export GNULIB_DIR=`pwd`/__build_gnulib/_build/gllib
 export CURL_DIR='/home/kele/curl-7.40.0-devel-mingw64'
 
 ./build.sh
-./configure --prefix="C:/opt" --disable-all-plugins CFLAGS="-Drestrict=__restrict -I${CURL_DIR}/include -I$GNULIB_DIR" LDFLAGS="-L$GNULIB_DIR -L${CURL_DIR}/bin" LIBS="-lgnu" --host="mingw32" \
+./configure --prefix="C:/opt" --disable-all-plugins \
+    CFLAGS="-Drestrict=__restrict -I${CURL_DIR}/include -I$GNULIB_DIR" \
+    LDFLAGS="-L$GNULIB_DIR -L${CURL_DIR}/bin" \
+    LIBS="-lgnu" \
+    --host="mingw32" \
     --enable-logfile \
     --enable-write_http
 cp ${GNULIB_DIR}/../config.h src/gnulib_config.h
